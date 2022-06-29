fff87bd9a742ea7fecf770b554d0d17612d611bb
Antonio Fernando Christophorus <antoniofernandochristophorus@dyn-118-139-86-94.its.monash.edu.au>
2022-06-29 17:09:06 +1000

    Reset Function

--- game.py
+++ game.py
@@ -47,6 +47,20 @@ class SnakeGameAI:
         self.score = 0
         self.food = None
         self._place_food()
+
+    def reset(self):
+        # init game state
+        self.direction = Direction.RIGHT
+
+        self.head = Point(self.w/2, self.h/2)
+        self.snake = [self.head,
+                      Point(self.head.x-BLOCK_SIZE, self.head.y),
+                      Point(self.head.x-(2*BLOCK_SIZE), self.head.y)]
+
+        self.score = 0
+        self.food = None
+        self._place_food()
+        self.frame_iteration = 0
         
     def _place_food(self):
         x = random.randint(0, (self.w-BLOCK_SIZE )//BLOCK_SIZE )*BLOCK_SIZE 
