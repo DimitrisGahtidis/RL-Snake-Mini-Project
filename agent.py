673de481b4e1cbfc62c7b9df5612501a72668aa9
Dimitris Gahtidis <DimitriGahtidis@gmail.com>
2022-06-29 17:39:58 +1000

    Implemented the train_long method and the remember method in the Agent class

--- agent.py
+++ agent.py
@@ -1,4 +1,3 @@
-from gettext import npgettext
 import torch
 import random
 import numpy as np
@@ -10,10 +9,20 @@ BATCH_SIZE = 1000
 LR = 0.001
 
 class Agent:
-    pass
+
+    def remember(self, state, action, reward, next_state, done):
+        self.memory.append((state, action, reward, next_state, done)) # append a tuples worth of information to the memory
+
+    def train_long_memory(self):
+            if len(self.memorty) > BATCH_SIZE:
+                mini_sample = random.sample(self.memory, BATCH_SIZE) # returns a batch size number of tuples
+            else: # if the memorty is lower than the batch size take the whole memory
+                mini_sample = self.memory
+            
+            states, actions, rewards, next_states, dones = zip(*mini_sample)
 
 def train():
     pass
-
+        
 if __name__ == '__main__':
     train()
