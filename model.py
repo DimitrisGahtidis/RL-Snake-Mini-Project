351a6aa5111cc55841279eceef3341934a6aee3b
Antonio <airantoniofernando@gmail.com>
2022-07-02 21:34:18 +1000

    Added Forward Function

--- model.py
+++ model.py
@@ -9,6 +9,11 @@ class Linear_QNet(nn.Module):
         super().__init__()
         self.linear1 = nn.Linear(input_size, hidden_size)
         self.linear2 = nn.Linear(hidden_size, output_size)
+    
+    def forward(self, x):
+        x = F.relu(self.linear1(x))
+        x = self.linear2(x)
+        return x
 
 class QTrainer:
     def __init__(self, model, lr, gamma):
